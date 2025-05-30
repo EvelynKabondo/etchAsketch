c1cde8c596b36cd1f2bb3daa0ad9c3fba9c873fb
Evelyn Kabondo <kabondoevie@gmail.com>
2025-05-30 20:37:08 +0200

    added console.log

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const container =document.getElementById('container');
 for (let i =0; i <256; i++) {
     const newdiv =document.createElement('div');
-    container.appendChild(newDiv);
+    container.appendChild(newdiv);
 }
+console.log(container);
